8d0f228c78f1cb53e207034458f1deacf8d9300d
Jamdu <Martinez.josh506@gmail.com>
2021-09-12 18:08:55 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scnr = new Scanner(System.in);
+        int year = Year.now().getValue();
+
+        //System.out.println(dtf.format(now));
+
+        System.out.println("What is your age?");
+        int age = scnr.nextInt();
+
+        System.out.println("At what age would you like to retire?");
+        int retire = scnr.nextInt();
+
+        int retireAge = retire - age;
+
+        if (retireAge < 0){
+            System.out.println("You can retire ya old coot!");
+            System.exit(0);
+        }
+
+        System.out.format("You have %d years left until you can retire.\n", retireAge);
+
+        int retireYear = year + retireAge;
+
+        System.out.format("It's " + year + ", so you can retire in %d.", retireYear);
+
+
+
+    }
+}
